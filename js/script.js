d2a4213406cc231d0a512198bfe339abae4d9bac
inamhaider111 <inamh4019@gmail.com>
2024-07-31 19:09:46 +0500

    design-live

---
+++ js/script.js
@@ -0,0 +1,277 @@
+(function($) {
+	
+	"use strict";
+	
+	// 1. preloader
+    $("#preloader").fadeOut(600);
+    $(".preloader-bg").delay(400).fadeOut(600);
+	
+	
+	//Update Header Style and Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			var siteHeader = $('.main-header');
+			var sticky_header = $('.main-header .sticky-header');
+			var scrollLink = $('.scroll-to-top');
+			if (windowpos > 55) {
+				siteHeader.addClass('fixed-header');
+				sticky_header.addClass("animated slideInDown");
+				scrollLink.fadeIn(300);
+			} else {
+				siteHeader.removeClass('fixed-header');
+				sticky_header.removeClass("animated slideInDown");
+				scrollLink.fadeOut(300);
+			}
+		}
+	}
+	
+	headerStyle();
+
+	//Banner Carousel Two
+	if ($('.banner-carousel').length) {
+		$('.banner-carousel').owlCarousel({
+			animateOut: 'fadeOut',
+		    animateIn: 'fadeIn',
+			loop:true,
+			margin:30,
+			nav:true,
+			smartSpeed: 15000,
+			mouseDrag:false,
+			touchDrag:false,
+			autoHeight: true,
+			autoplay: true,
+			autoplayTimeout:10000,
+			navText: [ '<span class="fa fa-long-arrow-alt-left"></span> prev', 'next<span class="fa fa-long-arrow-alt-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1024:{
+					items:1
+				},
+			}
+		});    		
+	}
+
+
+	//Fact Counter + Text Count
+	if($('.count-box').length){
+		$('.count-box').appear(function(){
+	
+			var $t = $(this),
+				n = $t.find(".count-text").attr("data-stop"),
+				r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+				
+			if (!$t.hasClass("counted")) {
+				$t.addClass("counted");
+				$({
+					countNum: $t.find(".count-text").text()
+				}).animate({
+					countNum: n
+				}, {
+					duration: r,
+					easing: "linear",
+					step: function() {
+						$t.find(".count-text").text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$t.find(".count-text").text(this.countNum);
+					}
+				});
+			}
+			
+		},{accY: 0});
+	}
+
+	//Testmonial Carousel
+	if ($('.testmonial-carousal').length) {
+		$('.testmonial-carousal').owlCarousel({
+			animateOut: 'fadeOut',
+		    animateIn: 'fadeIn',
+			loop:true,
+			margin:30,
+			nav:false,
+			dots:true,
+			items:2,
+			mouseDrag:true,
+			touchDrag:true,
+			autoplay: true,
+			autoplayTimeout:5000,
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1024:{
+					items:2
+				},
+			}
+		});    		
+	}
+
+	//Sponsors Carousel
+	if ($('.sponsors-carousel').length) {
+		$('.sponsors-carousel').owlCarousel({
+			loop:true,
+			margin:30,
+			nav:false,
+			smartSpeed: 950,
+			autoplay: true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				768:{
+					items:2
+				},
+				1024:{
+					items:3
+				},
+				1400:{
+					items:4
+				},
+			}
+		});    		
+	}
+
+
+	//Sortable Masonary with Filters
+	function enableMasonry() {
+		if($('.sortable-masonry').length){
+	
+			var winDow = $(window);
+			// Needed variables
+			var $container=$('.sortable-masonry .items-container');
+			var $filter=$('.filter-btns');
+	
+			$container.isotope({
+				filter:'*',
+				 masonry: {
+					columnWidth : 0.5
+				 },
+				animationOptions:{
+					duration:500,
+					easing:'linear'
+				}
+			});
+			
+	
+			// Isotope Filter 
+			$filter.find('li').on('click', function(){
+				var selector = $(this).attr('data-filter');
+	
+				try {
+					$container.isotope({ 
+						filter	: selector,
+						animationOptions: {
+							duration: 500,
+							easing	: 'linear',
+							queue	: false
+						}
+					});
+				} catch(err) {
+	
+				}
+				return false;
+			});
+	
+	
+			winDow.on('resize', function(){
+				var selector = $filter.find('li.active').attr('data-filter');
+
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 500,
+						easing	: 'linear',
+						queue	: false
+					}
+				});
+			});
+	
+	
+			var filterItemA	= $('.filter-btns li');
+	
+			filterItemA.on('click', function(){
+				var $this = $(this);
+				if ( !$this.hasClass('active')) {
+					filterItemA.removeClass('active');
+					$this.addClass('active');
+				}
+			});
+		}
+	}
+	
+	enableMasonry();
+	
+	// Scroll to a Specific Div
+	if($('.scroll-to-target').length){
+		$(".scroll-to-target").on('click', function() {
+			var target = $(this).attr('data-target');
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $(target).offset().top
+			 }, 1500);
+	
+		});
+	}
+	
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       false,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	}
+
+
+	/*==============================================          
+						Header Search               
+	==============================================*/
+	
+	$('.header-search a').on('click', function () {
+		$('.outer-box a > .fa-search').toggleClass('fa-times');
+	});
+
+
+
+/* ==========================================================================
+	When document is resize, do
+   ========================================================================== */
+   $(window).on('resize', function() {
+				
+	});
+
+
+/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+	});
+	
+/* ==========================================================================
+   When document is loading, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+	});	
+
+})(window.jQuery);
